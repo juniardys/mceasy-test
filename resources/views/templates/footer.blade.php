68098a75c81fb6e5babb9c9ff07ab38bb53958ef
Juniardy <juniardysetio@gmail.com>
2023-06-09 10:26:51 +0700

    bootstap template

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+<div class="navbar navbar-sm navbar-footer border-top">
+    <div class="container-fluid">
+        <span>&copy; {{ date('Y') }} <a href="https://themeforest.net/item/limitless-responsive-web-application-kit/13080328">Juniardy Setiowidayoga</a></span>
+    </div>
+</div>
+<!-- /footer -->
---
+++ resources/views/templates/main.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>McEasy Test</title>
+
+	<!-- Global stylesheets -->
+	<link href="{{ asset('assets/fonts/inter/inter.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('assets/icons/phosphor/styles.min.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('assets/css/ltr/all.min.css') }}" id="stylesheet" rel="stylesheet" type="text/css">
+	<!-- /global stylesheets -->
+
+	<!-- Core JS files -->
+	<script src="{{ asset('assets/demo/demo_configurator.js') }}"></script>
+	<script src="{{ asset('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+	<!-- /core JS files -->
+
+	<!-- Theme JS files -->
+	<script src="{{ asset('assets/js/vendor/visualization/d3/d3.min.js') }}"></script>
+	<script src="{{ asset('assets/js/vendor/visualization/d3/d3_tooltip.js') }}"></script>
+
+	<script src="{{ asset('assets/js/app.js') }}"></script>
+	<script src="{{ asset('assets/demo/pages/dashboard.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/streamgraph.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/sparklines.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/lines.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/areas.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/donuts.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/bars.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/progress.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/heatmaps.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/pies.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/bullets.js') }}"></script>
+	<!-- /theme JS files -->
+
+</head>
+
+<body>
+
+	@include('templates.navbar')
+
+	<!-- Page content -->
+	<div class="page-content">
+
+		@include('templates.sidebar')
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+
+			<!-- Inner content -->
+			<div class="content-inner">
+
+				@yield('content')
+
+                @include('templates.footer')
+
+			</div>
+			<!-- /inner content -->
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+
+</body>
+</html>
